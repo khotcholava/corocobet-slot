3edad3e46bab81dcfef026e18a99425f2281db1a
Akaki <31448057+khotcholava@users.noreply.github.com>
2024-03-20 22:36:23 +0400

    - implementing filter item component

---
+++ src/app/pages/slots/ui/filter-item/filter-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './filter-item.component.html',
+  styleUrl: './filter-item.component.scss'
+})
+export class FilterItemComponent {
+
+}
